c85eac10108a2baaae8750510c094059666d9f33
CassimClick <cassims44@gmail.com>
2023-11-01 16:22:24 +0300

    meta tag update

--- app/Views/Pages/Backend/Auth/Register.php
+++ app/Views/Pages/Backend/Auth/Register.php
@@ -7,8 +7,8 @@
     <meta charset="utf-8" />
     <title><?=$title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="MyraStudio" name="author" />
+    <meta content="Registration for  Business Investment Forum" name="description" />
+    <meta content="TIC" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <!-- App favicon -->
